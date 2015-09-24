d5bc10f7c1c8fedc3f42638becf2143e58a0dcbe
malachi26 <lyle.hart.3@gmail.com>
2015-09-24 15:28:06 -0500

    Initial

---
+++ CloserTo10/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CloserTo10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine(Closer(13, 15));    // output : 13
+            //Console.WriteLine(Closer(13, 13));   // output : 0
+            //Console.WriteLine(Closer(3, 5));      // output : 5
+            //Console.WriteLine(Closer(13, 7));    // output : 0
+            //Console.WriteLine(Closer(-3, -5));   // output : -3
+            //Console.WriteLine(Closer(-3, -1));   // output : -1
+            //Console.ReadLine();
+        }
+        public int Closer(int a, int b)
+        {
+            int calcA = a - 10;
+            int calcB = b - 10;
+            if (Math.Abs(calcA) == Math.Abs(calcB))
+            {
+                return 0;
+            }
+            else if ((a >= 10 || b >= 10) && a < b)
+            {
+                return a;
+            }
+            else if ((a >= 10 || b >= 10) && b < a)
+            {
+                return b;
+            }
+            else if (calcB > calcA && Math.Abs(a) != Math.Abs(b))
+            {
+                return b;
+            }
+            else if ((calcA < 0 || calcB < 0) && (calcA > calcB && Math.Abs(a) != Math.Abs(b)))
+            {
+                return a;
+            }
+            else return Closer(a, b);
+        }
+
+        public int Closer_LylesMug(int a, int b)
+        {
+            long distanceA = Math.Abs(10 - (long)a);
+            long distanceB = Math.Abs(10 - (long)b);
+            
+            if (distanceA == distanceB)
+            {
+                return 0;
+            }
+
+            return distanceA > distanceB ? b : a;
+        }
+
+        ///user2023861
+        ///
+
+        public int user2023861_Closer(int a, int b)
+        {
+            double avg = ((double)a + (double)b) / 2.0;
+            return ( a == b || avg == 10.0) ? 0 :
+                   avg > 10.0 ? Math.Min(a, b) : Math.Max(a, b);
+        }
+
+    }
+}
---
+++ CloserTo10_Test/UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CloserTo10;
+
+namespace CloserTo10_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(13, 15);
+            Assert.AreEqual(result, 13);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(13, 13);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+             var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(3, 5);
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+             var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(13, 7);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(-3, -5);
+            Assert.AreEqual(result, -3);
+        }
+
+        [TestMethod]
+        public void ELippertTest1()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(int.MinValue + 10, 0);
+            Assert.AreEqual(result, 0);
+        }
+
+
+        [TestMethod]
+        public void MaxA_BCloser()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(int.MaxValue, 0);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void MinA_BCloser()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(int.MinValue, 0);
+            Assert.AreEqual(result, 0);
+        }
+        [TestMethod]
+        public void MaxAndMin_Zero()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(int.MinValue, int.MaxValue);
+            Assert.AreEqual(result, int.MaxValue);
+        }
+
+        [TestMethod]
+        public void MinAMinB()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(int.MinValue, int.MinValue);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void MinMinus10AndZero_Zero()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.Closer_LylesMug(int.MaxValue, 0);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void User2023861_test1()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.user2023861_Closer(-10, -10);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void User2023861_test2()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.user2023861_Closer(-12, -10);
+            Assert.AreEqual(result, -10);
+        }
+
+        [TestMethod]
+        public void User2023861_test3()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.user2023861_Closer(10, -10);
+            Assert.AreEqual(result, 10);
+        }
+
+        [TestMethod]
+        public void User2023861_test4()
+        {
+            var Test = new CloserTo10.Program();
+            var result = Test.user2023861_Closer(30, -10);
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
